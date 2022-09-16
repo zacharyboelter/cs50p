366fa7a5a4ab2d57ec0dd4e0e4010235d7a5cd70
Zachary Boelter <zboelter@gmail.com>
2022-09-16 15:24:40 -0700

    fizzbuzz py

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -1,1 +1,13 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~  Conditionals ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+x = int(input('Is it a fizzbuzz? Enter a number '))
+
+if x % 3 == 0 and x % 5 == 0:
+    print('Fizzbuzz')
+elif x % 3 == 0:
+    print('Fizz')
+elif x % 5 == 0:
+    print('Buzz')
+else:
+    print('Nope')
