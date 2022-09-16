95918647fa9be8dbb41c2625aaef554d9a838bd6
Zachary Boelter <zboelter@gmail.com>
2022-09-16 15:55:32 -0700

    refactor is_even function

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -1,13 +1,31 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~  Conditionals ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-x = int(input('Is it a fizzbuzz? Enter a number '))
+# x = int(input('Is it a fizzbuzz? Enter a number '))
 
-if x % 3 == 0 and x % 5 == 0:
-    print('Fizzbuzz')
-elif x % 3 == 0:
-    print('Fizz')
-elif x % 5 == 0:
-    print('Buzz')
-else:
-    print('Nope')
+# if x % 3 == 0 and x % 5 == 0:
+#     print('Fizzbuzz')
+# elif x % 3 == 0:
+#     print('Fizz')
+# elif x % 5 == 0:
+#     print('Buzz')
+# else:
+#     print('Nope')
+
+
+def main():
+    x = int(input('What is x? '))
+    if is_even(x):
+        print('Even')
+    else:
+        print('Odd')
+
+def is_even(x):
+    # if x % 2 == 0:
+    #     return True
+    # else:
+    #     return False
+    
+    return True if x % 2 == 0 else False        #refactor 4 lines into 1
+
+main()
