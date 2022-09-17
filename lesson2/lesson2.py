87098a64f890f6b0918dd375929a1192ea5034ad
Zachary Boelter <zboelter@gmail.com>
2022-09-17 16:16:34 -0700

    build a calculator via interpreting a string and splitting

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -13,20 +13,73 @@
 #     print('Nope')
 
 
-def main():
-    x = int(input('What is x? '))
-    if is_even(x):
-        print('Even')
-    else:
-        print('Odd')
+# def main():
+#     x = int(input('What is x? '))
+#     if is_even(x):
+#         print('Even')
+#     else:
+#         print('Odd')
 
-def is_even(x):
-    # if x % 2 == 0:
-    #     return True
-    # else:
-    #     return False
+# def is_even(x):
+#     # if x % 2 == 0:
+#     #     return True
+#     # else:
+#     #     return False
     
-    # return True if x % 2 == 0 else False        #refactor 4 lines into 1
-    return x % 2 == 0                             #refactor if else into assumption
+#     # return True if x % 2 == 0 else False        #refactor 4 lines into 1
+#     return x % 2 == 0                             #refactor if else into assumption
+
+# main()
+
+
+
+ # ~~~~~~~~~~~~~~~~ Match ~~~~~~~~~~~~~~~~~~
+ 
+# name = input('Whats your name ')
+
+# match name:
+#     case 'bob' | 'bill' | 'billy':
+#         print('fuck you')
+#     case 'jack':
+#         print('not fuck you too')
+#     case _:
+#         print('I love you')
+
+
+# answer = str(input('What is the Great Answer of Life, the Universe and Everything? '))
+# a = answer.replace('-', ' ').lower().strip()
+
+# match a:
+#     case '42' | 'forty two':
+#         print('Yes')
+#     case _:
+#         print('No')
+
+
+def main():
+    expression = str(input("Enter an Expression of MATHS!!!! "))
+    x, y, z = expression.split()
+
+    x = float(x)
+    z = float(z)
+
+    match y:
+        case '+':
+            print(add(x,z))
+        case '-':
+            print(sub(x,z))
+        case '*':
+            print(mult(x,z))
+        case '/':
+            print(div(x,z))
+
+def add(x,z):
+    return x + z
+def sub(x,z):
+    return x - z
+def mult(x,z):
+    return round(x * z, 2)
+def div(x,z):
+    return round(x / z, 3)
 
 main()
