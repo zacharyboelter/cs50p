f5ddd4527db7cc9391325d0fb578b8d8fb893233
Zachary Boelter <zboelter@gmail.com>
2022-09-18 13:44:21 -0700

    recreated problem set 1 from yesterday

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -87,26 +87,93 @@
 # main()
 
 
+# def main():
+#     time = str(input('What time it is? '))
+#     hours, minutes = time.split(':')                    #split the string into two strings
+
+#     timeToEat = float(convert(hours, minutes))          #set the conversion function to a variable
+
+#     if timeToEat >= 7 and timeToEat <= 8:
+#         print('Bacon and eggs for breakky!!!!')
+#     elif timeToEat >=12 and timeToEat <= 13:
+#         print('Lets make a sandwich for lunch?')        #set conditionals for what meal of the day timeToEat falls into
+#     elif timeToEat >= 18 and timeToEat <=20:
+#         print('What do you want for dinner?')
+#     else:
+#         print('Go for a run, its no time to eat')
+
+
+# def convert(hours, minutes):
+#     hours = float(hours)                                #convert to float
+#     minutes = round(float(minutes) / 60, 2)             #convert minutes to float, divide that by 60 for the hour, round the float to 2nd place
+#     return float(hours) + float(minutes)                #return the minutes added to hours
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+
+# def main():
+#     math = str(input('what are you trying to solve? '))
+
+#     x, y, z = math.split()
+
+#     x = float(x)
+#     z = float(z)
+
+
+#     match y:
+#         case '+':
+#             print(add(x, z))
+#         case '-':
+#             print(subtract(x, z))
+#         case '*':
+#             print(multiply(x, z))
+#         case '/':
+#             print(divide(x, z))
+#         case _:
+#             print('Error, something went wrong')
+
+
+# def add(x, z):
+#     return x + z
+
+# def subtract(x, z):
+#     return x - z
+
+# def multiply(x, z):
+#     return round(x * z, 3)
+
+# def divide(x, z):
+#     return round(x / z, 3)
+
+# main()
+
+
+
 def main():
-    time = str(input('What time it is? '))
-    hours, minutes = time.split(':')                    #split the string into two strings
+    userTxt = str(input('What time is it? '))
+    hours, minutes = userTxt.split(':')
+    mealTime = convert(hours, minutes)
+    # print(mealTime)
 
-    timeToEat = float(convert(hours, minutes))          #set the conversion function to a variable
-
-    if timeToEat >= 7 and timeToEat <= 8:
-        print('Bacon and eggs for breakky!!!!')
-    elif timeToEat >=12 and timeToEat <= 13:
-        print('Lets make a sandwich for lunch?')        #set conditionals for what meal of the day timeToEat falls into
-    elif timeToEat >= 18 and timeToEat <=20:
-        print('What do you want for dinner?')
-    else:
-        print('Go for a run, its no time to eat')
-
+    if mealTime >= 7 and mealTime <= 8:
+        print('Its breakfast time!')
+    elif mealTime >= 12 and mealTime <= 14:
+        print('Lunch time!')
+    elif mealTime >= 18 and mealTime <=21:
+        print('Spanish dinner! Tapas baby!')
+    else: 
+        print('No soup for you!')
 
 def convert(hours, minutes):
-    hours = float(hours)                                #convert to float
-    minutes = round(float(minutes) / 60, 2)             #convert minutes to float, divide that by 60 for the hour, round the float to 2nd place
-    return float(hours) + float(minutes)                #return the minutes added to hours
+    hours = float(hours)
+    minutes = round(float(minutes) / 60, 2)
+    return hours + minutes
 
 
 if __name__ == "__main__":
