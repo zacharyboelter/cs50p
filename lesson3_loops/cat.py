855df05802243c1ed3d5f39e2f6c91a72e1377a2
Zachary Boelter <zboelter@gmail.com>
2022-09-19 16:15:16 -0700

    dictionary indexing

--- lesson3_loops/cat.py
+++ lesson3_loops/cat.py
@@ -2,11 +2,52 @@
 # print('meow')
 # print('meow')
 
-catSound = 0
+# catSound = 0
 
-while catSound < 5:
-    catSound += 1
-    # print('meow')
+# while catSound < 5:
+#     catSound += 1
+#     # print('meow')
 
-for _ in range(4):
-    print('woof')
+# for _ in range(4):
+#     print('woof')
+
+# print('moo\n' * 3, end='')
+
+# while True:
+#     n = int(input("What do you want n to be? "))
+#     if n > 0:
+#         break
+
+# for _ in range(n):
+#     print('Neiigghhhh')
+
+
+# def main():
+#     number = get_number()
+#     woof(number)
+
+# def get_number():
+#     while True:
+#         n = int(input('What is n? '))
+#         if n > 0:
+#             return n
+    
+
+# def woof(n):
+#     for _ in range(n):
+#         print('woof!!')
+
+
+
+students = ['Harry', 'Ron', 'Hermione', 'Draco']
+houses = ['Gryffindor', 'Gryffindor', 'Gryffindor', 'Slytherin']
+
+hogwarts = {
+    'Harry': 'Gryffindor', 
+    'Ron': 'Gryffindor',
+    'Hermione': 'Gryffindor',
+    'Draco': 'Slytherin'
+    }
+
+for student in hogwarts:
+    print(student, hogwarts[student], sep=" ~ House: ")
