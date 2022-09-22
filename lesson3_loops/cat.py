fd1c97ca53108f732dd5b119b7e3d1de8b0b63cb
Zachary Boelter <zboelter@gmail.com>
2022-09-22 14:05:31 -0700

    camelCase to snake_case program

--- lesson3_loops/cat.py
+++ lesson3_loops/cat.py
@@ -125,5 +125,28 @@
 
 
 
-main()
-    txt = input('')
+# main()
+#     txt = input('')
+
+
+# smallest = None
+# print("Before:", smallest)
+# for itervar in [3, 41, 12, 9, 74, 15]:
+#     if smallest is None or itervar < smallest:
+#         smallest = itervar
+#         break
+#     print("Loop:", itervar, smallest)
+# print("Smallest:", smallest)
+
+
+# counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
+# print(counts.get('kris', 0))
+
+text = input('Enter a camelCase phrase: ')
+
+for i in text:
+    if i.isupper():
+        new_text = '_' + i.lower()
+        i = new_text
+    print(i, end='')
+
